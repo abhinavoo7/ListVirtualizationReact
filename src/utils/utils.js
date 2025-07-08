31192e5c5daec4946251e8374f6dd58002fefecb
abhinavoo7 <gabhinav95a@gmail.com>
2025-07-08 14:17:13 +0530

    added function comments

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,11 @@
+/**
+ * @description
+ * Throttle a function so it only executes once every specified delay.
+ * Useful for limiting the rate at which a function can fire, such as scroll or resize events.
+ * @param {Function} func - The function to throttle.
+ * @param {number} delay - The delay in milliseconds between allowed executions.
+ * @returns {Function} A throttled version of the input function.
+ */
 export const throttle = (func, delay) => {
   let lastExecuted = 0;
 
